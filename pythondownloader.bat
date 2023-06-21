9999006154898a793ace2f039acf9aa5a042a11a
UltimaCodes <86365722+UltimaCodes@users.noreply.github.com>
2023-06-21 18:18:32 +0500

    Update pythondownloader.bat
    made the papamuzzy downloader useless

--- pythondownloader.bat
+++ pythondownloader.bat
@@ -16,7 +16,7 @@ if %errorlevel% == 0 (
 )
 echo Installing Python 3...
 REM Download the Python installer
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.9.6/python-3.9.6-amd64.exe', 'python-3.9.6-amd64')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.9.6/python-3.9.6-amd64.exe', 'python-3.9.6-amd64.exe')"
 REM Install Python with default options
 python-3.9.6-amd64.exe /quiet InstallAllUsers=1 PrependPath=1
 REM Check if the installation was successful
@@ -28,14 +28,14 @@ if %errorlevel% neq 0 (
 echo Python 3 has been installed successfully.
 
 :download_script
-echo Downloading Papamuzzy downloader script...
-REM Download the downloader script using curl
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/UltimaCodes/papamuzzy/main/papamuzzydownloader.pyw', 'papamuzzydownloader.pyw')"
-REM Run the downloader script
-if exist papamuzzydownloader.pyw (
-  python papamuzzydownloader.pyw
+echo Downloading Papamuzzy script...
+REM Download the script using curl
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/UltimaCodes/papamuzzy/main/papamuzzy.pyw', 'papamuzzy.pyw')"
+REM Run the script
+if exist papamuzzy.pyw (
+  python papamuzzy.pyw
 ) else (
-  echo Failed to download the Papamuzzy downloader script.
+  echo Failed to download the Papamuzzy script.
 )
 
 :end
