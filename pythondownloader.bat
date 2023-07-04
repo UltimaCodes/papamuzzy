280e415e1744438f8079f655ac4fde25dfc359a4
UltimaCodes <86365722+UltimaCodes@users.noreply.github.com>
2023-07-04 22:36:55 +0500

    Update pythondownloader.bat
    optimize

--- pythondownloader.bat
+++ pythondownloader.bat
@@ -9,23 +9,25 @@ if %errorlevel% == 0 (
     set version=!version:~0,3!
     if !version! geq 3.6 (
       echo Python 3 is already installed.
-      goto :install_modules
+    ) else (
+      echo An incompatible version of Python is installed.
+      goto :end
     )
-    echo An incompatible version of Python is installed.
   )
+) else (
+  echo Installing Python 3...
+  REM Download the Python installer
+  powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.9.6/python-3.9.6-amd64.exe', 'python-3.9.6-amd64.exe')"
+  REM Install Python with default options
+  python-3.9.6-amd64.exe /quiet InstallAllUsers=1 PrependPath=1
+  REM Check if the installation was successful
+  python --version >nul 2>&1
+  if %errorlevel% neq 0 (
+    echo Failed to install Python.
+    goto :end
+  )
+  echo Python 3 has been installed successfully.
 )
-echo Installing Python 3...
-REM Download the Python installer
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.9.6/python-3.9.6-amd64.exe', 'python-3.9.6-amd64.exe')"
-REM Install Python with default options
-python-3.9.6-amd64.exe /quiet InstallAllUsers=1 PrependPath=1
-REM Check if the installation was successful
-python --version >nul 2>&1
-if %errorlevel% neq 0 (
-  echo Failed to install Python.
-  goto :end
-)
-echo Python 3 has been installed successfully.
 
 REM Install curl and PowerShell if not installed
 echo Checking for curl...
