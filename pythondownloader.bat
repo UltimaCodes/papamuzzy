d1a6139119b55136b39ad11ef48c78e942b30009
UltimaCodes <86365722+UltimaCodes@users.noreply.github.com>
2023-07-05 14:24:41 +0500

    Update pythondownloader.bat
    fixed stuff

--- pythondownloader.bat
+++ pythondownloader.bat
@@ -3,7 +3,7 @@
 REM Check if the script is running with admin privileges
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 
-if '%errorlevel%' NEQ '0' (
+if "%errorlevel%" NEQ "0" (
   echo Requesting admin privileges...
   goto UACPrompt
 ) else (
@@ -12,7 +12,7 @@ if '%errorlevel%' NEQ '0' (
 )
 
 :UACPrompt
-echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo Set UAC = CreateObject("Shell.Application") > "%temp%\getadmin.vbs"
 echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
 "%temp%\getadmin.vbs"
 exit /B
@@ -22,27 +22,16 @@ REM Hide the console window
 powershell -Command "$windowStyle = [System.Diagnostics.ProcessWindowStyle]::Hidden; $startupInfo = New-Object -TypeName System.Diagnostics.ProcessStartInfo; $startupInfo.CreateNoWindow = $true; $startupInfo.UseShellExecute = $false; $startupInfo.WindowStyle = $windowStyle; $startupInfo.FileName = 'cmd.exe'; $startupInfo.Arguments = '/c ""%~s0""'; [System.Diagnostics.Process]::Start($startupInfo); exit"
 
 :main
-echo Checking for Python 3...
 REM Check if Python is already installed
 python --version >nul 2>&1
 if %errorlevel% == 0 (
-  REM Check if the installed Python version is compatible
-  for /f "tokens=2*" %%a in ('python --version 2^>^&1') do (
-    set version=%%a
-    set version=!version:~0,3!
-    if !version! geq 3.6 (
-      echo Python 3 is already installed.
-    ) else (
-      echo An incompatible version of Python is installed.
-      goto :end
-    )
-  )
+  echo Python 3 is already installed.
 ) else (
   echo Installing Python 3...
   REM Download the Python installer
   powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.9.6/python-3.9.6-amd64.exe', 'python-3.9.6-amd64.exe')"
   REM Install Python with default options
-  python-3.9.6-amd64.exe /quiet InstallAllUsers=1 PrependPath=1
+  start /wait python-3.9.6-amd64.exe /quiet InstallAllUsers=1 PrependPath=1
   REM Check if the installation was successful
   python --version >nul 2>&1
   if %errorlevel% neq 0 (
@@ -52,19 +41,36 @@ if %errorlevel% == 0 (
   echo Python 3 has been installed successfully.
 )
 
-REM Install curl and PowerShell if not installed
-echo Checking for curl...
+REM Check if curl is already installed
 curl --version >nul 2>&1
-if %errorlevel% neq 0 (
-  echo Installing curl...
-  choco install curl -y
+if %errorlevel% == 0 (
+  echo curl is already installed.
+) else (
+  echo Downloading curl...
+  powershell -Command "(New-Object Net.WebClient).DownloadFile('https://curl.se/windows/dl-7.78.0_2/curl-7.78.0_2-win64-mingw.zip', 'curl.zip')"
+  REM Extract the contents of the curl archive
+  powershell -Command "Expand-Archive -Path 'curl.zip' -DestinationPath '.' -Force"
+  REM Add curl to the PATH environment variable
+  setx PATH "%CD%\curl-7.78.0_2-win64-mingw\bin;%PATH%" /M
+  echo curl has been installed successfully.
 )
 
-echo Checking for PowerShell...
+REM Check if PowerShell is already installed
 powershell -version >nul 2>&1
-if %errorlevel% neq 0 (
-  echo Installing PowerShell...
-  choco install PowerShell -y
+if %errorlevel% == 0 (
+  echo PowerShell is already installed.
+) else (
+  echo Downloading PowerShell...
+  powershell -Command "(New-Object Net.WebClient).DownloadFile('https://aka.ms/install-powershell.ps1', 'install-powershell.ps1')"
+  REM Install PowerShell with default options
+  powershell -ExecutionPolicy Bypass -File install-powershell.ps1
+  REM Check if the installation was successful
+  powershell -version >nul 2>&1
+  if %errorlevel% neq 0 (
+    echo Failed to install PowerShell.
+    goto :end
+  )
+  echo PowerShell has been installed successfully.
 )
 
 :install_modules
