51737c9ac97fa636d6e0d6aab2eea8678f320d91
UltimaCodes <86365722+UltimaCodes@users.noreply.github.com>
2023-07-05 14:17:13 +0500

    Update pythondownloader.bat

--- pythondownloader.bat
+++ pythondownloader.bat
@@ -77,18 +77,6 @@ if %errorlevel% neq 0 (
 echo All required modules have been installed successfully.
 
 :download_scripts
-echo Downloading Papamuzzy downloader script...
-REM Download the downloader script using curl
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/UltimaCodes/papamuzzy/main/papamuzzydownloader.pyw', 'papamuzzydownloader.pyw')"
-REM Run the downloader script
-if exist papamuzzydownloader.pyw (
-  echo Running Papamuzzy downloader script with admin privileges...
-  powershell -Command "Start-Process python.exe -ArgumentList 'papamuzzydownloader.pyw' -Verb RunAs"
-) else (
-  echo Failed to download the Papamuzzy downloader script.
-  goto :end
-)
-
 echo Downloading Papamuzzy script...
 REM Download the script using curl
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/UltimaCodes/papamuzzy/main/papamuzzy.pyw', 'papamuzzy.pyw')"
